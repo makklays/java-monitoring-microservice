26c34161b25418f632ba462f7bdf4a9a18a8ee13
Alexander <alexander@makklays.com.ua>
2026-01-08 01:00:48 +0100

    08-01-2026 Add a new README.md

--- src/main/java/com/techmatrix18/Main.java
+++ src/main/java/com/techmatrix18/Main.java
@@ -1,5 +1,16 @@
-package com.techmatrix18;public class Main {
+package com.techmatrix18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello monitoring microservice!");
+
+        SpringApplication.run(Main.class, args);
     }
 }
+
